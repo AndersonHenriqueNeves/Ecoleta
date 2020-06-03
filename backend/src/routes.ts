0b39acce2c88123a07c94a091fe41d5b02a67b2e
Lamithy <anderson001749@gmail.com>
2020-06-03 18:34:07 -0300

    backend

---
+++ backend/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsControler = new ItemsController();
+
+routes.get('/items', itemsControler.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
